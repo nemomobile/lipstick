1714cfc8ad4759384fb401cdea25cee6b2cbb2a9
Aki Koskinen <aki.koskinen@nokia.com>
2010-03-02 17:10:31 +0200

    New: scenario "Opening Launcher from Quick Launch Bar"
    RevBy: Eetu Lehmusvuo
    Details: added needed step definitions for scenario
    "Opening Launcher from Quick Launch Bar"

--- testsuites/acceptancetests/features/step_definitions/dui-2397_quick_launch_bar.rb
+++ testsuites/acceptancetests/features/step_definitions/dui-2397_quick_launch_bar.rb
@@ -13,6 +13,18 @@ Given /^switcher is opened$/ do
     verify_not(3) { @app.DuiModalSceneWindow(:name => 'LauncherWindow') }
 end
 
+Given /^launcher is closed$/ do
+    verify_not(3) { @app.DuiModalSceneWindow(:name => 'LauncherWindow') }
+end
+
+When /^I tap on the launcher open\/close button$/ do
+    @app.DuiButton(:name => 'ToggleLauncherButton').tap
+end
+
 Then /^Quick Launch Bar is visible$/ do
     @app.QuickLaunchBar(:visible => 'true')
+end
+
+Then /^launcher is opened$/ do
+    @app.DuiModalSceneWindow(:name => 'LauncherWindow')
 end
