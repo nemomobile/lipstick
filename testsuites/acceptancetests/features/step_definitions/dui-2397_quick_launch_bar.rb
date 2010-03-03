57f123a529d80becd26dbc2b8780c919171dfd51
Aki Koskinen <aki.koskinen@nokia.com>
2010-03-03 12:00:45 +0200

    New: scenario "Showing Quick Launch Bar in Launcher"
    RevBy: Eetu Lehmusvuo
    Details: added needed step definitions for scenario
    "Showing Quick Launch Bar in Launcher"

--- testsuites/acceptancetests/features/step_definitions/dui-2397_quick_launch_bar.rb
+++ testsuites/acceptancetests/features/step_definitions/dui-2397_quick_launch_bar.rb
@@ -4,6 +4,14 @@ Before do
     @sut = MATTI.sut(:Id => 'sut_qt_maemo')
 end
 
+After do
+    # Close the launcher if it's visible
+    begin
+        @app.DuiModalSceneWindow(:name => 'LauncherWindow', :__timeout => 1)
+        @app.DuiButton(:name => 'ToggleLauncherButton').tap
+    rescue
+    end
+end
 
 Given /^duihome is running$/ do
     @app = @sut.application(:name => 'duihome')
@@ -15,6 +23,15 @@ end
 
 Given /^launcher is closed$/ do
     verify_not(3) { @app.DuiModalSceneWindow(:name => 'LauncherWindow') }
+end
+
+Given /^I have opened the launcher$/ do
+    # Open the launcher if it's not visible
+    begin
+        @app.DuiModalSceneWindow(:name => 'LauncherWindow', :__timeout => 1)
+    rescue
+        @app.DuiButton(:name => 'ToggleLauncherButton').tap
+    end
 end
 
 When /^I tap on the launcher open\/close button$/ do
