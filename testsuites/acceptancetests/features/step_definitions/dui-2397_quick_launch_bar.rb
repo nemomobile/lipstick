295e27273a23c81947f3a9664a1e70c638897be8
Aki Koskinen <aki.koskinen@nokia.com>
2010-03-03 20:07:54 +0200

    New: scenario "Having a full Quick Launch Bar"
    RevBy: Eetu Lehmusvuo

--- testsuites/acceptancetests/features/step_definitions/dui-2397_quick_launch_bar.rb
+++ testsuites/acceptancetests/features/step_definitions/dui-2397_quick_launch_bar.rb
@@ -34,9 +34,15 @@ Given /^I have opened the launcher$/ do
     end
 end
 
+Given /^I have configured application "([^\"]*)" to the slot "([^\"]*)" of Quick Launch Bar$/ do |application, slot|
+    setSlotConfiguration(slot, application)
+end
+
+
 When /^I tap on the launcher open\/close button$/ do
     @app.DuiButton(:name => 'ToggleLauncherButton').tap
 end
+
 
 Then /^Quick Launch Bar is visible$/ do
     @app.QuickLaunchBar(:visible => 'true')
@@ -44,4 +50,47 @@ end
 
 Then /^launcher is opened$/ do
     @app.DuiModalSceneWindow(:name => 'LauncherWindow')
+end
+
+Then /^Quick Launch Bar slot "([^\"]*)" contains application "([^\"]*)" icon$/ do |slot, application|
+    ensureConfigurationUpdated()
+
+    index = slot.to_i - 1
+    qlbObject = @app.QuickLaunchBar.child(:name => 'QuickLaunchBarButton', :__index => index)
+    verify_equal('LauncherButton') { qlbObject.type }
+    verify_equal(applicationDesktopEntryName(application)) { qlbObject.attribute('desktopEntry') }
+end
+
+
+$configuration = {}
+$configurationDirty = false
+
+def ensureConfigurationUpdated()
+    if $configurationDirty
+        output = ''
+
+        $configuration.each_pair do |s, a|
+            output += "[#{s}]\n"
+            output += "desktopFile=/tmp/#{a}.desktop\n"
+        end
+
+        target = File.expand_path('~/.config/duihome/quicklaunchbar.data')
+        File.open(target, 'w') { |f|
+            f.write(output)
+        }
+
+        $configurationDirty = false
+
+        # Give some time for the application to react on the change
+        sleep 2
+    end
+end
+
+def setSlotConfiguration(slot, application)
+    $configuration[slot] = application
+
+    # Copy the widgetsgallery desktop entry to a temporary file
+    File.copy('/usr/share/applications/widgetsgallery.desktop', '/tmp/' + application + '.desktop')
+
+    $configurationDirty = true
 end
