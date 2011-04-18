4c210a4fb8ec52b62803dac2e78ad178b08cb67e
Pankaj Saharan <pankaj.saharan@nokia.com>
2011-04-18 14:11:04 +0300

    Fixes: NB#247807 - Legal bug in meegotouchhome
    RevBy: TrustMe

--- src/libmeegotouchhome/layoutvisualizationwrapper.h
+++ src/libmeegotouchhome/layoutvisualizationwrapper.h
@@ -1,18 +1,21 @@
-/*
- * This file is part of mhome
- *
- * Copyright (C) 2010 Nokia Corporation and/or its subsidiary(-ies).
- * All rights reserved.
- *
- * Contact: Tomas Junnonen <tomas.junnonen@nokia.com>
- *
- * This software, including documentation, is protected by copyright
- * controlled by Nokia Corporation. All rights are reserved. Copying,
- * including reproducing, storing, adapting or translating, any or all of
- * this material requires the prior written consent of Nokia Corporation.
- * This material also contains confidential information which may not be
- * disclosed to others without the prior written consent of Nokia.
- */
+/***************************************************************************
+**
+** Copyright (C) 2010 Nokia Corporation and/or its subsidiary(-ies).
+** All rights reserved.
+** Contact: Nokia Corporation (directui@nokia.com)
+**
+** This file is part of mhome.
+**
+** If you have questions regarding the use of this file, please contact
+** Nokia at directui@nokia.com.
+**
+** This library is free software; you can redistribute it and/or
+** modify it under the terms of the GNU Lesser General Public
+** License version 2.1 as published by the Free Software Foundation
+** and appearing in the file LICENSE.LGPL included in the packaging
+** of this file.
+**
+****************************************************************************/
 
 #ifndef LAYOUTVISUALIZATIONWRAPPER_H
 #define LAYOUTVISUALIZATIONWRAPPER_H
