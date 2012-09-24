ea59cf9b9f40e2e033c55083622ae946afec6c54
Robin Burchell <robin+git@viroteck.net>
2012-09-24 11:53:57 +0200

    remove incorrect copyright information

--- src/components/windowinfo.h
+++ src/components/windowinfo.h
@@ -1,13 +1,6 @@
 /***************************************************************************
 **
 ** Copyright (C) 2010 Nokia Corporation and/or its subsidiary(-ies).
-** All rights reserved.
-** Contact: Nokia Corporation (directui@nokia.com)
-**
-** This file is part of mhome.
-**
-** If you have questions regarding the use of this file, please contact
-** Nokia at directui@nokia.com.
 **
 ** This library is free software; you can redistribute it and/or
 ** modify it under the terms of the GNU Lesser General Public
