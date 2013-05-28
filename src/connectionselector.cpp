724f6f5e5468a4b601457072ff934c659cd2c71c
Vesa Halttunen <vesa.halttunen@jollamobile.com>
2013-05-28 11:22:50 +0200

    Connection selector rework

--- src/connectionselector.cpp
+++ src/connectionselector.cpp
@@ -1,50 +1,31 @@
-/****************************************************************************
+/***************************************************************************
 **
-** Copyright (C) 2013 Jolla Ltd
-** Contact: lorn.potter@jollamobile.com
+** Copyright (C) 2013 Jolla Ltd.
+** Contact: Robin Burchell <robin.burchell@jollamobile.com>
 **
+** This file is part of lipstick.
 **
-** GNU Lesser General Public License Usage
-** This file may be used under the terms of the GNU Lesser
-** General Public License version 2.1 as published by the Free Software
-** Foundation and appearing in the file LICENSE.LGPL included in the
-** packaging of this file.  Please review the following information to
-** ensure the GNU Lesser General Public License version 2.1 requirements
-** will be met: http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html.
+** This library is free software; you can redistribute it and/or
+** modify it under the terms of the GNU Lesser General Public
+** License version 2.1 as published by the Free Software Foundation
+** and appearing in the file LICENSE.LGPL included in the packaging
+** of this file.
 **
 ****************************************************************************/
-
-#include "connectionselector.h"
 
 #include <QApplication>
 #include <QDeclarativeView>
 #include <QDeclarativeContext>
 #include <QDesktopWidget>
-#include <QGraphicsObject>
-
-
+#include <QTimer>
 #include "utilities/closeeventeater.h"
-#include "notifications/notificationmanager.h"
+#include "connectionselector.h"
 
 ConnectionSelector::ConnectionSelector(QObject *parent) :
     QObject(parent),
-    window(0),
-    currentNotification(0)
+    window(new QDeclarativeView())
 {
-    window = new QDeclarativeView();
-    window->setAttribute(Qt::WA_TranslucentBackground);
-    window->setAttribute(Qt::WA_X11NetWmWindowTypeMenu);
-    window->setWindowTitle("Connection");
-    window->setResizeMode(QDeclarativeView::SizeRootObjectToView);
-    window->viewport()->setAutoFillBackground(false);
-    window->rootContext()->setContextProperty("connectionSelector", this);
-    window->rootContext()->setContextProperty("initialSize", QApplication::desktop()->screenGeometry(window).size());
-    window->setSource(QUrl("qrc:/qml/ConnectionSelector.qml"));
-    window->installEventFilter(new CloseEventEater(this));
-    QObject *rootObject = window->rootObject();
-    QObject::connect(rootObject,SIGNAL(canceled()),this,SLOT(onCanceled()));
-    QObject::connect(rootObject,SIGNAL(connectionRequested()),
-                     this,SLOT(onConnectionRequest()));
+    QTimer::singleShot(0, this, SLOT(createWindow()));
 }
 
 ConnectionSelector::~ConnectionSelector()
@@ -52,11 +33,23 @@ ConnectionSelector::~ConnectionSelector()
     delete window;
 }
 
+void ConnectionSelector::createWindow()
+{
+    window->setAttribute(Qt::WA_TranslucentBackground);
+    window->setAttribute(Qt::WA_X11DoNotAcceptFocus);
+    window->setAttribute(Qt::WA_X11NetWmWindowTypeMenu);
+    window->setWindowTitle("Connection");
+    window->setResizeMode(QDeclarativeView::SizeRootObjectToView);
+    window->viewport()->setAutoFillBackground(false);
+    window->rootContext()->setContextProperty("initialSize", QApplication::desktop()->screenGeometry(window).size());
+    window->rootContext()->setContextProperty("connectionSelector", this);
+    window->setSource(QUrl("qrc:/qml/ConnectionSelector.qml"));
+    window->installEventFilter(new CloseEventEater(this));
+}
+
 void ConnectionSelector::setWindowVisible(bool visible)
 {
-    window->resize(QApplication::desktop()->screenGeometry(window).size());
     if (visible) {
-
         if (!window->isVisible()) {
             window->showFullScreen();
             emit windowVisibleChanged();
@@ -70,32 +63,4 @@ void ConnectionSelector::setWindowVisible(bool visible)
 bool ConnectionSelector::windowVisible() const
 {
     return window != 0 && window->isVisible();
-}
-
-void ConnectionSelector::onConnectionRequest()
-{
-    setWindowVisible(true);
-}
-
-void ConnectionSelector::onCanceled()
-{
-    setWindowVisible(false);
-}
-
-void ConnectionSelector::showNotification(const QString &message,const QString &type)
-{
-    if (message.isEmpty())
-        return;
-
-    NotificationManager *manager = NotificationManager::instance();
-    QVariantHash hints;
-    QString cat = "x-nemo.connectionselector.";
-    cat.append(type);
-    hints.insert(NotificationManager::HINT_CATEGORY, cat);
-    hints.insert(NotificationManager::HINT_PREVIEW_BODY, message);
-    uint tmpUint;
-
-    tmpUint = manager->Notify(qApp->applicationName(), 0, QString(), QString(), QString(),
-                              QStringList(), hints, -1);
-    currentNotification = tmpUint;
 }
