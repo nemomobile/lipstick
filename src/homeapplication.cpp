ff9a547cf7e372624a91aa396169aaafaaa3ae0c
Matt Vogt <matthew.vogt@jollamobile.com>
2015-03-16 11:12:32 +1000

    [lipstick] Set translated application name
    The application name is needed to group notifications.

--- src/homeapplication.cpp
+++ src/homeapplication.cpp
@@ -67,7 +67,6 @@ HomeApplication::HomeApplication(int &argc, char **argv, const QString &qmlPath)
     , originalSigTermHandler(signal(SIGTERM, quitSignalHandler))
     , homeReadySent(false)
 {
-    setApplicationName("Lipstick");
     // TODO: autogenerate this from tags
     setApplicationVersion(VERSION);
 
@@ -77,6 +76,10 @@ HomeApplication::HomeApplication(int &argc, char **argv, const QString &qmlPath)
     QTranslator *translator = new QTranslator(this);
     translator->load(QLocale(), "lipstick", "-", "/usr/share/translations");
     installTranslator(translator);
+
+    // Set the application name, as used in notifications
+    //% "System"
+    setApplicationName(qtTrId("qtn_ap_lipstick"));
 
     // Initialize the QML engine
     qmlEngine = new QQmlEngine(this);
